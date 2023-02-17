7426fe31d3ad7dfc8b74a21da8d980c253f82b96
carlo.loguercio <carlo.log@gmail.com>
2023-02-17 14:27:10 +0100

    add comments for HttpError class

--- network_client/__brick__/network/errors/http_error.dart
+++ network_client/__brick__/network/errors/http_error.dart
@@ -1,7 +1,14 @@
+/// Represents an HTTP error, which is thrown when an HTTP request results in a
+/// response with an error status code.
 class HttpError {
+  /// The status code of the HTTP response that caused the error.
   final int statusCode;
+
+  /// The error message returned by the HTTP response that caused the error.
   final String message;
 
+  /// Creates a new instance of [HttpError] with the specified status code and
+  /// error message.
   HttpError({required this.statusCode, required this.message});
 
   @override
