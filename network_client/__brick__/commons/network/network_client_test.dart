7e0431202b48918c968b212c7c72392d95aec490
carlo.loguercio <carlo.log@gmail.com>
2023-02-24 19:18:43 +0100

    feat: add test file for network_client

---
+++ network_client/__brick__/commons/network/network_client_test.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../network/network_client.dart';
+import '../../network/network_client_impl.dart';
+
+void main() {
+  group('NetworkClient', () {
+    late NetworkClient networkClient;
+    setUp(() {
+      networkClient = NetworkClientImpl("https://jsonplaceholder.typicode.com");
+    });
+
+    test('GET', () async {
+      final response = await networkClient.get("/posts/1");
+      expect(response.statusCode, 200);
+      final post = jsonDecode(response.body);
+      expect(post['id'], 1);
+    });
+
+    test('POST', () async {
+      final response = await networkClient.post("/posts", body: {
+        "title": "foo",
+        "body": "bar",
+        "userId": "1"
+      });
+      expect(response.statusCode, 201);
+      final post = jsonDecode(response.body);
+      expect(post['title'], 'foo');
+    });
+
+    test('PUT', () async {
+      final response = await networkClient.put("/posts/1", body: {
+        "id": "1",
+        "title": "foo",
+        "body": "bar",
+        "userId": "1"
+      });
+      expect(response.statusCode, 200);
+      final post = jsonDecode(response.body);
+      expect(post['title'], 'foo');
+    });
+
+    test('DELETE', () async {
+      final response = await networkClient.delete("/posts/1");
+      expect(response.statusCode, 200);
+    });
+  });
+}
